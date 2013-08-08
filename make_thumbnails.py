2638f5823a08c1b91da786936e25471158129b9f
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2013-08-08 14:51:52 +0200

    ENH thumbnails are now squared

--- make_thumbnails.py
+++ make_thumbnails.py
@@ -2,6 +2,10 @@ import os
 
 for i in range(1, 22):
     entry = 'entry%d' % i
-    os.system(
-        'convert -size x100 2013/%s/%s.png thumbnails/%s.png' %
-        (entry, entry, entry))
+    os.system('convert 2013/%s/%s.png -trim thumbnails/%s.png' %
+              (entry, entry, entry))
+    cmd = ('convert thumbnails/%s.png -set '
+           'option:size "%%[fx:min(w,h)]x%%[fx:min(w,h)]" '
+           ' xc:none +swap -gravity center -composite thumbnails/%s.png' %
+           (entry, entry))
+    os.system(cmd)
